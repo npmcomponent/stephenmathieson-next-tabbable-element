0e01b8df850ff713ff389af207ebd999676c1ba2
Stephen Mathieson <smath23@gmail.com>
2014-02-19 15:10:07 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var isFocousable = require('is-focusable');
+var descendants = require('descendants');
+
+module.exports = nextTabbableElement;
+
+/**
+ * Find the element in the tab order after `root`,
+ * defaulting to the `body`.
+ *
+ * @api public
+ * @param {HTMLElement} [root]
+ * @return {HTMLElement}
+ */
+
+function nextTabbableElement(root) {
+  var elements = descendants(document.body);
+  var i = root
+    ? elements.indexOf(root) + 1
+    : 0;
+
+  for (var len = elements.length; i < len; i++) {
+    if (!isFocousable(elements[i])) continue;
+    // while `<div tabindex="-1" />` can receive focus, it's
+    // not in the native order
+    if ('-1' != elements[i].getAttribute('tabindex'))
+      return elements[i];
+  }
+
+  return null;
+}
---
+++ test/next-tabbable-element.js
@@ -0,0 +1,67 @@
+
+describe('next-tabbable-element', function () {
+  var assert = require('assert');
+  var next = require('next-tabbable-element');
+
+  var fixture;
+
+  // ensure we know what the first element in the tab order is
+  before(function () {
+    var first = document.createElement('div');
+    first.tabIndex = 0;
+    first.innerHTML = first.id = 'yo';
+    document.body.insertBefore(first, document.body.firstChild);
+  });
+
+  beforeEach(function () {
+    fixture = document.createElement('div');
+    fixture.innerHTML =
+        '<button id="a">a</button>'
+      + '<div tabindex="-1" id="b">b</div>'
+      + '<a href="#" id="c">c</a>'
+      + '<div>'
+      + '  <div>'
+      + '    <div>'
+      + '      <div>'
+      + '        <div>'
+      + '          <div>'
+      + '            <div>'
+      + '              <div>'
+      + '                <a href="#" id="d">d</a>'
+      + '              </div>'
+      + '            </div>'
+      + '          </div>'
+      + '        </div>'
+      + '      </div>'
+      + '    </div>'
+      + '  </div>'
+      + '</div>';
+    document.body.appendChild(fixture);
+  });
+
+  afterEach(function () {
+    document.body.removeChild(fixture);
+  });
+
+  it('should return the next element in the tab order', function () {
+    var a = document.querySelector('#a');
+    var c = document.querySelector('#c');
+    assert(c == next(a));
+  });
+
+  it('should handle deeply nested elements', function () {
+    var c = document.querySelector('#c');
+    var d = document.querySelector('#d');
+    assert(d == next(c));
+  });
+
+  it('should return null if on the last tabbable element', function () {
+    var d = document.querySelector('#d');
+    assert(null == next(d));
+  });
+
+  it('should default to the body', function () {
+    var yo = document.querySelector('#yo');
+    assert(yo == next());
+  });
+});
